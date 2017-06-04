bcf4bc47d96b14968e14964db86794ba13c091de
thisisaaroland <aaron@mapzen.com>
2017-06-04 12:58:32 -0700

    JS scaffolding for api.test.js

--- javascript/mapzen.api.test.js
+++ javascript/mapzen.api.test.js
@@ -1,20 +1,53 @@
-function mapzen_api_test(){
+(function(f){
 
-	var submit = document.getElementById("submit");
-	submit.onclick = function(){
+        if (typeof exports === "object" && typeof module !== "undefined"){
+		module.exports = f();
+        }
+	
+        else if (typeof define === "function" && define.amd){
+		define([],f);
+        }
+	
+        else {
+		var g;
 		
-		try {
+		if (typeof window!=="undefined") {
+			g=window;
+		} else if (typeof global!=="undefined") {
+			g=global;
+		} else if (typeof self!=="undefined") {
+			g=self;
+		} else {
+			g=this;
+		}
+
+		g.mapzen = g.mapzen || {};
+		g.mapzen.api = g.mapzen.api || {};
+		g.mapzen.api.test = g.mapzen.api.test = f();
+	}
+	
+}(function(){
+
+	var self = {
+
+		'init': function(){
 			
+			var submit = document.getElementById("submit");
+			submit.onclick = self.test;
+		},
+
+		'test': function(){
+		
     			var api_endpoint = document.body.getAttribute("data-api-endpoint");    
     			var api_key = document.body.getAttribute("data-api-key");
 			
     			if (! api_key){
-				alert("API key is undefined");
+				alert("API key is not defined");
     				return false;
 			}
 			
     			if (! api_endpoint){
-				alert("API endpoint is undefined");
+				alert("API endpoint is not defined");
     				return false;
 			}
 			
@@ -24,7 +57,7 @@ function mapzen_api_test(){
 			
     			var q = document.getElementById("q");
 			q = q.value;
-
+			
 			if (q.indexOf('?') != -1){
 				q = q.replace("?", "");
 			}
@@ -34,11 +67,10 @@ function mapzen_api_test(){
 			var count_q = q.length;
 			
 			for (var i=0; i < count_q; i++){
-				
 				var pair = q[i].split("=");
 				params[ pair[0] ] = pair[1];
 			}
-			
+				
 			var str_params = [];
 			
 			for (k in params) {
@@ -46,61 +78,61 @@ function mapzen_api_test(){
 				
 				k = encodeURIComponent(k);
 				v = encodeURIComponent(v);			  
-
+				
 				str_params.push([k, v].join("="));
 			}
-
+			
 			str_params = str_params.join("&");
-
+			
 			var url = api_endpoint + "?" + str_params;
 			
+			self.execute_request(url);
+			return false;
+		},
+
+		'execute_request': function(url){
+				
 			var onload = function(rsp){
-
+					
 				var target = rsp.target;
-
+				
 				if (target.readyState != 4){
 					return;
 				}
-
+				
 				var status_code = target['status'];
 				var status_text = target['statusText'];
-
+				
 				var raw = target['responseText'];
-
+				
 				var body = [
 					url,
 					status_code + " " + status_text,
 					raw,
 				];
-
+				
 				body = body.join("\n");
-
+				
 				var pre = document.createElement("pre");
 				pre.setAttribute("class", "test-results");				
 				pre.innerText = body;
 				
     				var results = document.getElementById("results");
 				results.innerHTML = "";
-
+				
 				results.appendChild(pre);
 			};
 			
 			var req = new XMLHttpRequest();
-
+			
 			req.addEventListener("load", onload);		
-
 			req.open("GET", url, true);
 			req.send();
-
+				
     			var results = document.getElementById("results");
 			results.innerText = "GET " + url;
-		}
-
-		catch(e){
-			console.log(e);
-		}
-		
-		return false;
+		},
 	};
-	
-}    
+
+	return self;
+}));
